bf193d72fa005bd446fc1bb3fca560629a75d413
GEOVANAAPROGRAMMER <rm99646@fiap.com.br>
2023-08-22 21:55:22 -0300

    Criação de dicionários.

---
+++ dicionario_python.py
@@ -0,0 +1,9 @@
+tabela = {}
+                    
+                    #pts, vitorias, gols pro, gols contra
+tabela['botafogo'] = [16,6,34,10]
+tabela['corinthias'] = [25, 8, 79, 1]
+tabela['palmeiras'] = [12, 4, 1, 10]
+tabela['santos'] = [5, 1, 4, 9]
+
+print(tabela)
